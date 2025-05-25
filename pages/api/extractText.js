c4d3e37588bb5b140eaa0bef75d2d2e9bab20d3b
etasick <etasick@gmail.com>
2025-05-25 20:24:38 +0100

    added originalUrl to worker call

--- pages/api/extractText.js
+++ pages/api/extractText.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
     const workerResponse = await fetch("https://talkingpages-audio-worker-parser.etasick.workers.dev/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ summary }),
+      body: JSON.stringify({ summary,originalUrl: url }),
     });
 
     if (!workerResponse.ok) {
